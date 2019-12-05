891cc54d0c3292a17db2d4f6123456355ebcd730
Ray Bergholm <raybergholm@gmail.com>
2019-12-05 11:00:06 +0200

    update read_file version

--- src/utils/file.py
+++ src/utils/file.py
@@ -1,24 +1,21 @@
 #!/usr/bin/env python3
 
 
-def read_file(filepath, line_delimiter=None):
-    try:
-        with open(filepath, "r") as file_stream:
-            data = file_stream.read()
-            if line_delimiter != None:
-                data = data.split(line_delimiter)
+def read_file(filepath, line_delimiter="\n", do_for_each_line=None, do_after_file_read=None):
+    with open(filepath, "r") as file_stream:
+        data = file_stream.read()
+        if line_delimiter != None:
+            data = data.split(line_delimiter)
+            if do_for_each_line and callable(do_for_each_line):
+                data = [do_for_each_line(line) for line in data]
+
+    if do_after_file_read and callable(do_after_file_read):
+        return do_after_file_read(data)
+    else:
         return data
-    except OSError as error:
-        print(str(error))
 
 
 def save_file(filepath, data):
-    try:
-        with open(filepath, "w+") as file_stream:
-            try:
-                for entry in data:
-                    file_stream.write(str(entry))
-            except TypeError:
-                file_stream.write(entry)
-    except OSError as error:
-        print(str(error))
+    with open(filepath, "w+") as file_stream:
+        for entry in data:
+            file_stream.write(entry)

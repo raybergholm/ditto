3c278a3e58d5ccb9a362869cd7e6a19a842c7cf7
Ray Bergholm <raybergholm@gmail.com>
2019-12-05 11:59:05 +0200

    fix incorrect args passed to convert functions

--- src/ditto.py
+++ src/ditto.py
@@ -9,38 +9,33 @@ from utils.file import read_file, save_file
 def convert_and_save():
     args = parse_arguments()
 
-    delimiter = args.delimiter if args.delimiter != None else ";"
-    field_list = args.fields
-
     input_filetype = check_filetype(args.input_filepath)
     output_filetype = "json" if input_filetype == "csv" else "csv"
 
     if input_filetype == "json":
-        # input_data = read_file(args.input_filepath)
-        data = convert_json_to_csv(args.input_filepath, delimiter, field_list)
+        data = convert_json_to_csv(args.input_filepath, delimiter=args.delimiter,
+                                   newline=args.newline, field_list=args.field_list)
     elif input_filetype == "csv":
-        # input_data = read_file(args.input_filepath, line_delimiter=args.newline)
-        data = convert_csv_to_json(args.input_filepath, delimiter, field_list)
+        data = convert_csv_to_json(args.input_filepath, delimiter=args.delimiter,
+                                   newline=args.newline, field_list=args.field_list)
     else:
         print("File extension not supported (check if it was a .json or .csv file)")
         return
 
-    output_filepath = args.output_filepath if args.output_filepath else "%s.%s" % (
+    # If no output filepath was supplied, use the same filepath as the input and just switch the filetype
+    output_filepath = args.output_filepath if args.output_filepath else "{0}.{1}".format(
         args.input_filepath.split(".")[0], output_filetype)
 
     save_file(output_filepath, data)
     print("File saved to %s" % output_filepath)
 
 
-def convert():
-    pass
-
-def convert_json_to_csv(filepath, delimiter=";", newline="\n", fieldnames=None):
-    data = from_json_file(filepath, fieldnames)
+def convert_json_to_csv(filepath, delimiter=";", newline="\n", field_list=None):
+    data = from_json_file(filepath, field_list)
     return json_to_csv(data)
 
 
-def convert_csv_to_json(filepath, delimiter=";", newline="\n", fieldnames=None):
+def convert_csv_to_json(filepath, delimiter=";", newline="\n", field_list=None):
     (header, data) = from_csv_file(filepath, delimiter, newline)
     return csv_to_json(header, data)
 
@@ -55,8 +50,8 @@ def parse_arguments():
                         default=";", help="delimiter to use (default: semicolon)")
     parser.add_argument("-n", "--newline", dest="newline", action="store",
                         default="\n", help="newline type (default: \"\\n\")")
-    parser.add_argument("-f", "--fields", dest="fields", action="store",
-                        help="fields to use as an filter (FIELD1;FIELD2;FIELD3)")
+    parser.add_argument("-f", "--fields", dest="field_list", action="store",
+                        default=None, help="fields to use as an filter (FIELD1;FIELD2;FIELD3)")
     args = parser.parse_args()
     return args
 

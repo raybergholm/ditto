888b2e4011a2bc51641b956a767863fd63af71a2
Ray Bergholm <raybergholm@gmail.com>
2021-10-01 15:15:30 +0300

    remove unused function

--- src/utils/converter.py
+++ src/utils/converter.py
@@ -21,18 +21,6 @@ def to_json(data):
     return json.dumps(data)
 
 
-def to_csv_old(data, delimiter, newline):
-    header = [key for key in data[0].keys()]
-    output_data = []
-
-    output_data.append("{0}{1}".format(delimiter.join(header), newline))
-    for row in data:
-        # This kind of looks superfluous when filtering happens before this step, but this is actually more about enforcing a set field order if you get objects with different key ordering
-        entry = [str(row[field]) if field in row else "" for field in header]
-        output_data.append("{0}{1}".format(delimiter.join(entry), newline))
-    return output_data
-
-
 def to_csv(data, delimiter, quotechar):
     fields = [key for key in data[0].keys()]
 
